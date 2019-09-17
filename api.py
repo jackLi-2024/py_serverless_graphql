26b30c37899745a586bcc3669017ae53a6c2cf16
lijiacaigit <44719983+lijiacaigit@users.noreply.github.com>
2019-09-17 10:59:31 +0800

    Update api.py

--- api.py
+++ api.py
@@ -26,7 +26,10 @@ from api_model.test import Test
 from controlers.test import deal_test
 
 
-class Query(graphene.ObjectType):
+class QueryTest(graphene.ObjectType):
+    class Meta:
+        description = '查询类接口'
+        name = "SearchApi"
     name = graphene.Field(Name, name=graphene.String())
     num = graphene.List(Num, num=graphene.Int())
     test = graphene.List(Test)
@@ -44,6 +47,28 @@ class Query(graphene.ObjectType):
     def resolve_test(self, info):
         result = deal_test()
         return result
+    
+    
+class MutateTest(graphene.ObjectType):
+    class Meta:
+        description = '修改操作类接口'
+        name = "OperateApi"
 
+    name = graphene.Field(Name, name=graphene.String())
+    num = graphene.List(Num, num=graphene.Int())
 
-schema = graphene.Schema(Query)
+    def resolve_name(self, info, name):
+        return {"name": name}
+
+    def resolve_num(self, info, num):
+        result = []
+        for i in range(10):
+            if i < num:
+                result.append({"num": i})
+        return result
+
+    def resolve_test(self, info):
+        result = deal_test()
+        return result
+
+schema = graphene.Schema(QueryTest,mutation=MutateTest)
